be73c6b37432142c06241418a3a6b5aec9fecdf1
KissMate0101 <matekiss2008@gamil.com>
2026-02-18 09:09:42 +0100

    projekt elkezdve

---
+++ Tavugras/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavugras
+{
+    internal class Student
+    {
+        private string name;
+        private string city;
+        private int result;
+
+        public Student(string name, string city, int result)
+        {
+            this.name = name;
+            this.city = city;
+            this.result = result;
+        }
+    }
+}
